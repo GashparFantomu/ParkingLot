e758fb5a24a4fd5de18cc3d2945ae8a8e4730a1a
GashparFantomu <128855619+GashparFantomu@users.noreply.github.com>
2025-11-11 16:55:43 +0200

    some help for my teammates

---
+++ src/main/java/org/example/parkinglot/entities/Car.java
@@ -0,0 +1,40 @@
+package org.example.parkinglot.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "licensePlate")
+    private String licensePlate;
+
+    @Column(name = "parkingSpot")
+    private String parkingSpot;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
